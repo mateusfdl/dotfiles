b68490ef22ace0b713e8b42e1f7a4284979c4ced
Matheus Lima <matheus.limastack@outlook.com>
2021-12-08 19:23:49 -0300

    Add nvim-tree lua package settings

---
+++ private_dot_config/nvim/lua/settings/nvim-tree.lua
@@ -0,0 +1,48 @@
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  auto_close          = false,
+  open_on_tab         = false,
+  update_to_buf_dir   = {
+    enable = true,
+    auto_open = true,
+  },
+  hijack_cursor       = false,
+  update_cwd          = false,
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+
+  view = {
+    width = 30,
+    height = 30,
+    side = 'left',
+    auto_resize = true,
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  }
+}
+
+vim.g["NERDTreeIgnore"] = [[
+    \ '.git'  
+    ]]
+vim.cmd("highlight NvimTreeFolderIcon guibg=blue")
