2b6e8de35505d9361fdfaa3f7a4689f2892c9586
Matheus Lima <matheus.limastack@outlook.com>
2022-01-26 09:21:06 -0300

    [Nvim] add new style to lsp documentation box

--- private_dot_config/nvim/lua/settings/lsp.lua
+++ private_dot_config/nvim/lua/settings/lsp.lua
@@ -1,8 +1,9 @@
 vim.o.completeopt = "menuone,noselect"
 
 require('settings.lsp.solargraph')
-require('settings.lsp.tsserver')
 require('settings.lsp.golang')
+require('settings.lsp.haskell')
+require('settings.lsp.tsserver')
 
 require'compe'.setup {
   enabled = true;
@@ -13,11 +14,17 @@ require'compe'.setup {
   throttle_time = 80;
   source_timeout = 200;
   incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = false;
-
+  max_abbr_width = 150;
+  max_kind_width = 150;
+  max_menu_width = 150;
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' },
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  };
   source = {
     path = true;
     buffer = true;
@@ -68,6 +75,3 @@ end
 
 vim.cmd("autocmd BufWritePre *.rb lua vim.lsp.buf.formatting_sync(nil, 100)")
 vim.cmd("autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 100)")
-
-
-vim.g["completion_enable_snippet"] = "UltiSnips"
