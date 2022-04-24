9070e82561ad002f7c0444f6e6c38d73867cbebc
Matheus Lima <matheus.limastack@outlook.com>
2022-04-24 01:44:00 -0300

    [Nvim] Fix solargraph formatting settings

--- private_dot_config/nvim/lua/settings/lsp/solargraph.lua
+++ private_dot_config/nvim/lua/settings/lsp/solargraph.lua
@@ -1,31 +1,17 @@
 local lspconfig = require'lspconfig'
 
-local function attacher(client)
-  print('Attaching LSP: ' .. client.name)
-end
-
-lspconfig.jsonls.setup{
-  on_attach = attacher
-}
-
 lspconfig.solargraph.setup{
   settings = {
     solargraph = {
       commandPath = '/Users/joaomatheusfurtadodelima/.asdf/shims/solargraph',
-      diagnostics = true,
-      completion = true,
-      autoformat = true,
-      autoimport = true,
-      lint = true,
-      formating = true,
-      documentation = true,
-      hover = true,
-      signature = true,
-      references = true,
-      rename = true,
       logLevel = 'warn',
     }
   },
-
   on_attach = attacher
 }
+
+local function attacher(client, bufnr)
+  print('Attaching LSP: ' .. client.name)
+  client.resolved_capabilities.document_formatting = false
+  client.resolved_capabilities.document_range_formatting = false
+end
