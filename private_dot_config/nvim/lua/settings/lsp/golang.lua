aae5a94999dc9f398986fabc3171f28c6b272c41
Matheus Lima <matheus.limastack@outlook.com>
2022-04-24 01:43:42 -0300

    [Nvim] add gopls server setup as lsp

--- private_dot_config/nvim/lua/settings/lsp/golang.lua
+++ private_dot_config/nvim/lua/settings/lsp/golang.lua
@@ -1,1 +1,20 @@
-require'lspconfig'.golangci_lint_ls.setup{}
+local lspconfig = require'lspconfig'
+
+local function attacher(client)
+  print('Attaching LSP: ' .. client.name)
+  client.resolved_capabilities.document_formatting = false
+  client.resolved_capabilities.document_range_formatting = false
+end
+
+lspconfig.gopls.setup {
+    cmd = {"gopls", "serve"},
+    settings = {
+      gopls = {
+        analyses = {
+          unusedparams = true,
+        },
+        staticcheck = true,
+      },
+    },
+    on_attach = attacher,
+  }
