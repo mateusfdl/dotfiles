f33ef598d996d04155ae45602d9b4c5078d35609
Matheus Lima <matheus.limastack@outlook.com>
2022-04-27 18:24:57 -0300

    [Nvim] add Organize command at Ts lsp settingas

--- private_dot_config/nvim/lua/settings/lsp/tsserver.lua
+++ private_dot_config/nvim/lua/settings/lsp/tsserver.lua
@@ -1,11 +1,28 @@
 local nvim_lsp = require("lspconfig")
 
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+    title = ""
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+
 nvim_lsp.tsserver.setup {
-    on_attach = attacher
+    on_attach = function(client, bufnr)
+      print('Attaching LSP: ' .. client.name)
+      client.resolved_capabilities.document_formatting = false
+      client.resolved_capabilities.document_range_formatting = false
+
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":OrganizeImports<CR>", {silent = true})
+    end,
+    commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize Imports"
+    }
+  }
 }
 
-local function attacher(client, bufnr)
-  print('Attaching LSP: ' .. client.name)
-  client.resolved_capabilities.document_formatting = false
-  client.resolved_capabilities.document_range_formatting = false
-end
