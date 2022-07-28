cb8bed010ad6513340e487dfa502eefffee58a8b
Matheus Lima <matheus.limastack@outlook.com>
2022-07-28 19:00:12 -0300

    [Nvim] add norg to treesitter

--- private_dot_config/nvim/lua/settings/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/settings/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "norg" },
   highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = { "c" },  -- list of language that will be disabled
+    enable = true,
+    disable = { "c" },
   },
 }
