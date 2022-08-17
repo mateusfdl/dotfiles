7a7b2b76ef5d85e02d51fd14fd3487af659a8de3
Matheus Lima <matheus.limastack@outlook.com>
2022-08-17 19:12:01 -0300

    [Nvim] add Kanagawa theme setup

---
+++ private_dot_config/nvim/lua/settings/theme.lua
@@ -0,0 +1,12 @@
+require('kanagawa').setup({
+    undercurl = true,
+    commentStyle = "italic",
+    functionStyle = "NONE",
+    specialReturn = true,
+    specialException = true, 
+    transparent = false,
+    dimInactive = false,
+    colors = {},
+    overrides = {},
+})
+
