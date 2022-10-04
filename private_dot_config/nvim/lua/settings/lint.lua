00f77c852439ee22f6c757a4989a5942f0bc6696
Matheus Lima <matheus.limastack@outlook.com>
2022-10-04 08:52:01 -0300

    [Nvim] add lint plugin config file

---
+++ private_dot_config/nvim/lua/settings/lint.lua
@@ -0,0 +1,11 @@
+do 
+  local lint = require('lint')
+  lint.linters_by_ft = {
+    dockerfile = {'hadolint',}
+  }
+
+  vim.api.nvim_create_autocmd({'BufWritePost', 'BufEnter', 'BufLeave'}, {
+    group = vim.api.nvim_create_augroup('lint', { clear = true }),
+    callback = function() lint.try_lint() end,
+  })
+end
