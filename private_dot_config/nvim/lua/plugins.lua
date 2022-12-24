996a3af0f6ff0dbedcd033403c01a4cb191e81fb
Matheus Lima <matheus.limastack@outlook.com>
2022-12-24 18:31:36 -0300

    removed null-ls

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -7,7 +7,6 @@ require("packer").startup(function()
     use { 'dracula/vim', as = 'dracula' } 
     use { 'nvim-lua/popup.nvim' } 
     use { 'nvim-lua/plenary.nvim' } 
-    use { 'kyazdani42/nvim-tree.lua' } 
     use { 'nvim-telescope/telescope.nvim' } 
     use { 'christoomey/vim-tmux-runner' } 
     use { 'yggdroot/indentline' } 
@@ -24,12 +23,13 @@ require("packer").startup(function()
     use { 'jose-elias-alvarez/nvim-lsp-ts-utils' }
     use { 'mattn/efm-langserver'}
     use { 'glepnir/dashboard-nvim' }
-    use { 'rebelot/kanagawa.nvim' }
+    use { 'rebelot/kanagawa.nvim', commit = 'fc2e308' }
     use { 'folke/lsp-colors.nvim' }
-    use { 'jose-elias-alvarez/null-ls.nvim' }
     use { 'glepnir/lspsaga.nvim' }
     use { 'voldikss/vim-floaterm' }
     use { "nvim-neorg/neorg" }
     use { "mfussenegger/nvim-lint" }
     use { "https://github.com/nat-418/boole.nvim" }
+    use { "mateusfdl/spongebob-stupid-nvim" }
+    use { 'nvim-tree/nvim-tree.lua', requires = { 'nvim-tree/nvim-web-devicons' } }
 end)
