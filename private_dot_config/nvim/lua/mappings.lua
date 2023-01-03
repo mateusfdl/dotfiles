79d7374827a0decf7c0fd864500c3b22637afb31
Matheus Lima <matheus.limastack@outlook.com>
2023-01-03 18:33:59 -0300

    add new vmode block movements

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -3,6 +3,12 @@ require "utils.mappings"
 local M = {}
 
 function M.system()
+    nnoremap("<c-j>", ":m .+1<CR>==")
+    nnoremap("<c-k>", ":m .-2<CR>==")
+    inoremap("<c-j>", "<Esc>:m .+1<CR>==gi")
+    inoremap("<c-k>", "<Esc>:m .-2<CR>==gi")
+    vnoremap("<c-j>", ":m '>+1<CR>gv=gv")
+    vnoremap("<c-k>", ":m '<-2<CR>gv=gv")
     nnoremap("<Left>", ":echoe 'this --> h'<CR>")
     nnoremap("<Right>", ":echoe 'this --> l'<CR>")
     nnoremap("<Up>", ":echoe 'this --> j'<CR>")
