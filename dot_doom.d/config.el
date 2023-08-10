78c6441c52c2b99982660c42c6a5311824779c94
Matheus Lima <matheus.limastack@outlook.com>
2023-08-09 21:58:25 -0300

    add new emacs config

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -1,14 +1,12 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 (setq user-full-name "Matheus Lima"
-      user-mail-address "matheus.limastack@outlook.com"
+      user-mail-address "matheus.limastack@gmail.com"
 
       doom-theme 'doom-dracula
 
       display-line-numbers-type t
 
-      doom-font(font-spec :family "Hack Nerd Font Mono" :size 12)
+      doom-font(font-spec :family "IosevkaSS04 Nerd Font Mono" :size 12)
 
       org-directory "~/org/")
-
-
