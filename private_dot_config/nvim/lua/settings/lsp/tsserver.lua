10a2072c7fa92ec65318c673ff7a2d0e8a9df45a
Matheus Lima <matheus.limastack@outlook.com>
2023-08-10 02:03:32 -0300

    fix organize imports

--- private_dot_config/nvim/lua/settings/lsp/tsserver.lua
+++ private_dot_config/nvim/lua/settings/lsp/tsserver.lua
@@ -1,16 +1,19 @@
+require("utils.mappings")
 local M = {}
 
-function M.on_attach(client, bufnr)
-    local buff = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf)
-    local params = { command = "_typescript.organizeImports", arguments = { buff }, title = "" }
-    vim.lsp.buf.execute_command(params)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":OrganizeImports<CR>", {silent = true})
-    commands = {
-      OrganizeImports = {
-        organize_imports,
-        description = "Organize Imports"
-      }
+_G.organize_imports = function ()
+  vim.lsp.buf.execute_command {
+    command = '_typescript.organizeImports',
+    arguments = {
+      vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
     }
+  }
+end
+vim.cmd('command! OrganizeImports lua organize_imports()')
+
+function M.on_attach(client, bufnr)
+  buf_set_keybind(bufnr, "n", "gs",  ":OrganizeImports<CR>")
 end
 
 return M
+
