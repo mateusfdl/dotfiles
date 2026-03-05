36744df457276f25a03938eedcb09674639a17ad
Matheus Lima <matheus.limastack@gmail.com>
2026-03-05 18:57:39 -0300

    add custom qs plugin path into qs env var

--- nix/modules/optional/hyprland.nix
+++ nix/modules/optional/hyprland.nix
@@ -6,11 +6,14 @@
     xwayland.enable = true;
   };
 
-  security.pam.services.quickshell = {};
+  security.pam.services.quickshell = { };
 
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
-    QML2_IMPORT_PATH = "${pkgs.qt6.qt5compat}/lib/qt-6/qml";
+    QML2_IMPORT_PATH = builtins.concatStringsSep ":" [
+      "${pkgs.qt6.qt5compat}/lib/qt-6/qml"
+      "$HOME/.config/quickshell/plugin/build/qml"
+    ];
     XCURSOR_THEME = "macOS";
     XCURSOR_SIZE = "24";
     WLR_NO_HARDWARE_CURSORS = "1";
