df0dc2e232df4df51d434a357f075b46cfde045d
Matheus Lima <matheus.limastack@gmail.com>
2026-04-08 13:11:42 -0300

    remove redundant session vars and stale activation script
    Remove three items that no longer serve a purpose:
    
    - XDG_CACHE_HOME: was set to $HOME/.cache, which is already the XDG
      default and adds no value.
    - THUMBNAIL_CACHE_DIRECTORY: non-standard variable, not consumed by any
      tool in the current setup.
    - activationScripts.clearVideoThumbnails: one-time workaround to purge
      stale video thumbnails left over from the totem → ffmpegthumbnailer
      migration. The cache has long been cleared; running this on every
      nixos-rebuild is unnecessary and hardcodes /home/matheus.

--- nix/modules/optional/nautilus.nix
+++ nix/modules/optional/nautilus.nix
@@ -1,11 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   environment.systemPackages = with pkgs; [
     nautilus
     nautilus-python
     ffmpegthumbnailer
-    totem
     libheif
+    vlc
+    loupe
   ];
 
   services.gvfs.enable = true;
@@ -15,35 +16,98 @@
     [Thumbnailer Entry]
     TryExec=ffmpegthumbnailer
     Exec=ffmpegthumbnailer -i %i -o %o -s %s -f
-    MimeType=video/jpeg;video/mp4;video/mpeg;video/quicktime;video/x-ms-asf;video/x-ms-wmv;video/x-msvideo;video/x-flv;video/x-matroska;video/webm;video/x-mpeg;video/x-mp4;video/x-avi;
+    MimeType=video/jpeg;video/mp4;video/mpeg;video/quicktime;video/x-ms-asf;video/x-ms-wmv;video/x-msvideo;video/x-flv;video/x-matroska;video/webm;video/x-mpeg;video/x-mp4;video/x-avi;video/3gp;video/3gpp;video/asf;video/avi;video/divx;video/dv;video/fli;video/flv;video/mp2t;video/mp4v-es;video/msvideo;video/ogg;video/vivo;video/vnd.divx;video/vnd.avi;video/vnd.rn-realvideo;video/vnd.vivo;video/x-anim;video/x-flc;video/x-fli;video/x-fll;video/x-m4v;video/x-mpeg2;video/x-ms-asf-plugin;video/x-ogm+ogg;video/x-theora;video/x-theora+ogg;
   '';
 
-  environment.etc."xdg/thumbnailers/totem.thumbnailer".text = ''
-    [Thumbnailer Entry]
-    TryExec=totem-video-thumbnailer
-    Exec=totem-video-thumbnailer -s %s %i %o
-    MimeType=video/3gp;video/3gpp;video/asf;video/avi;video/divx;video/dv;video/fli;video/flv;video/mp2t;video/mp4;video/mp4v-es;video/mpeg;video/msvideo;video/ogg;video/quicktime;video/vivo;video/vnd.divx;video/vnd.avi;video/vnd.rn-realvideo;video/vnd.vivo;video/webm;video/x-anim;video/x-avi;video/x-flc;video/x-fli;video/x-fll;video/x-flv;video/x-m4v;video/x-matroska;video/x-mpeg;video/x-mpeg2;video/x-ms-asf;video/x-ms-asf-plugin;video/x-ms-wmv;video/x-msvideo;video/x-ogm+ogg;video/x-theora;video/x-theora+ogg;
+  environment.etc."xdg/gtk-3.0/gtk.css".text = ''
+    .nautilus-canvas-item {
+      border: none;
+      box-shadow: none;
+    }
+
+    .nautilus-canvas-item.thumbnail,
+    .nautilus-canvas-item.thumbnail::before,
+    .nautilus-canvas-item.thumbnail::after {
+      border: none;
+      box-shadow: none;
+    }
+
+    NautilusCanvasItem {
+      border: none;
+    }
+
+    .thumbnail {
+      border: none;
+      outline: none;
+    }
+
+    NautilusIconContainer {
+      border: none;
+    }
   '';
 
-  programs.dconf.profiles.user.databases = [{
-    settings = {
-      "org/gnome/nautilus/preferences" = {
-        show-image-thumbnails = "always";
-        thumbnail-limit = 104857600;
-        show-thumbnails = true;
-      };
-      "org/gnome/desktop/thumbnailers" = {
-        disable-all = false;
-        disable = [];
-      };
-      "org/gnome/nautilus/list-view" = {
-        use-tree-view = false;
-      };
-    };
-  }];
+  environment.etc."xdg/gtk-4.0/gtk.css".text = ''
+    .nautilus-canvas-item {
+      border: none;
+      box-shadow: none;
+    }
 
-  environment.sessionVariables = {
-    XDG_CACHE_HOME = "$HOME/.cache";
-    THUMBNAIL_CACHE_DIRECTORY = "$HOME/.cache/thumbnails";
+    .nautilus-canvas-item.thumbnail,
+    .nautilus-canvas-item.thumbnail::before,
+    .nautilus-canvas-item.thumbnail::after {
+      border: none;
+      box-shadow: none;
+    }
+
+    NautilusCanvasItem {
+      border: none;
+    }
+
+    .thumbnail {
+      border: none;
+      outline: none;
+    }
+
+    NautilusIconContainer {
+      border: none;
+    }
+  '';
+
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/nautilus/preferences" = {
+          show-image-thumbnails = "always";
+          thumbnail-limit = lib.gvariant.mkUint64 104857600;
+          show-thumbnails = true;
+        };
+        "org/gnome/desktop/thumbnailers" = {
+          disable-all = false;
+          disable = lib.gvariant.mkEmptyArray lib.gvariant.type.string;
+        };
+        "org/gnome/nautilus/list-view" = {
+          use-tree-view = false;
+        };
+      };
+    }
+  ];
+
+  xdg.mime.defaultApplications = {
+    "image/jpeg" = "org.gnome.Loupe.desktop";
+    "image/png" = "org.gnome.Loupe.desktop";
+    "image/gif" = "org.gnome.Loupe.desktop";
+    "image/webp" = "org.gnome.Loupe.desktop";
+    "image/bmp" = "org.gnome.Loupe.desktop";
+    "image/tiff" = "org.gnome.Loupe.desktop";
+    "image/svg+xml" = "org.gnome.Loupe.desktop";
+    "image/x-icon" = "org.gnome.Loupe.desktop";
+    "image/heic" = "org.gnome.Loupe.desktop";
+    "image/heif" = "org.gnome.Loupe.desktop";
+    "image/avif" = "org.gnome.Loupe.desktop";
+    "image/raw" = "org.gnome.Loupe.desktop";
+    "image/x-canon-cr2" = "org.gnome.Loupe.desktop";
+    "image/x-nikon-nef" = "org.gnome.Loupe.desktop";
+    "image/x-sony-arw" = "org.gnome.Loupe.desktop";
   };
+
 }
