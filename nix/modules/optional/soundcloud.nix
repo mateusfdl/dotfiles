719b239e0c0ca1eaaec78c0961d18dedc361242e
Matheus Lima <matheus.limastack@gmail.com>
2026-04-08 12:57:12 -0300

    add soundcloud bin

--- nix/modules/optional/soundcloud.nix
+++ nix/modules/optional/soundcloud.nix
@@ -1,16 +1,20 @@
 { pkgs, ... }:
 let
-  soundcloud = pkgs.makeDesktopItem {
+  soundcloudBin = pkgs.writeShellScriptBin "soundcloud" ''
+    exec ${pkgs.brave}/bin/brave --app=https://soundcloud.com "$@"
+  '';
+
+  soundcloudDesktop = pkgs.makeDesktopItem {
     name = "soundcloud";
     desktopName = "SoundCloud";
     comment = "SoundCloud Music Streaming";
     icon = "soundcloud";
-    exec = "${pkgs.brave}/bin/brave --app=https://soundcloud.com";
+    exec = "soundcloud";
     categories = [ "AudioVideo" "Audio" ];
     startupNotify = true;
     startupWMClass = "soundcloud.com";
   };
 in
 {
-  environment.systemPackages = [ soundcloud ];
+  environment.systemPackages = [ soundcloudBin soundcloudDesktop ];
 }
