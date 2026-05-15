a3c3cb3807c4589e1062f1b0179e15ec9a39dc40
Matheus Lima <matheus.limastack@gmail.com>
2026-05-15 20:27:37 -0300

    enable nix gc on desktop

--- nix/modules/common/nix.nix
+++ nix/modules/common/nix.nix
@@ -3,4 +3,10 @@
   nixpkgs.config.allowUnfree = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
