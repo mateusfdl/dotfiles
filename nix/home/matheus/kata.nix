e62134e66a753728ff4b2059f75978a164a86b3c
Matheus Lima <matheus.limastack@gmail.com>
2026-07-04 18:33:37 -0300

    bump kata to 1.3.0 and enable rules

--- nix/home/matheus/kata.nix
+++ nix/home/matheus/kata.nix
@@ -6,6 +6,26 @@
     enable = true;
     settings = {
       ratchet = true;
+      enabled = [
+        "context-first-param"
+        "error-strings"
+        "max-complexity"
+        "max-function-length"
+        "max-nesting"
+        "no-any"
+        "no-as-any"
+        "no-builtin-print"
+        "no-comments"
+        "no-console"
+        "no-defer-in-loop"
+        "no-empty-interface"
+        "no-generic-error"
+        "no-init-func"
+        "no-panic"
+        "no-swallowed-errors"
+        "no-weak-assertions"
+        "simple-repositories"
+      ];
     };
   };
 }
