ee09cb549fcd3954e805c50d00d6ef2faa171680
Matheus Lima <matheus.limastack@gmail.com>
2026-09-08 12:59:37 -0300

    update nix

--- nix/modules/nixos/optional/obs.nix
+++ nix/modules/nixos/optional/obs.nix
@@ -1,10 +1,17 @@
 { pkgs, ... }:
+let
+  obs-shaderfilter = pkgs.obs-studio-plugins.obs-shaderfilter.overrideAttrs {
+    version = "2.6.0-unstable-2026-05-21";
+    src = pkgs.fetchFromGitHub {
+      owner = "exeldro";
+      repo = "obs-shaderfilter";
+      rev = "851c61eb4e293704360068d7bb8c93a251bb5718";
+      hash = "sha256-owQ8bQriyLrq4ekNUy7TPS3jnOzCXBNex5M5EHk2K6M=";
+    };
+  };
+in
 {
-  environment.systemPackages = with pkgs; [
-    (wrapOBS {
-      plugins = with obs-studio-plugins; [
-        obs-shaderfilter
-      ];
-    })
+  environment.systemPackages = [
+    (pkgs.wrapOBS { plugins = [ obs-shaderfilter ]; })
   ];
 }
